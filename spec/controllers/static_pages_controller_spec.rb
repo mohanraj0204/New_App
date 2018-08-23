95283dfa17f028de6c60087547ff28d4dbb933d8
mohanraj0204 <mohan.raj@fourkites.com>
2018-08-23 18:22:42 +0530

    initialize pushed

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+RSpec.describe StaticPagesController do
+
+  describe "GET 'home'" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Sample App')
+    end
+  end
+
+end
