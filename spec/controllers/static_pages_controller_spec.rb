2fd303398ed158d09ef0e8b65b82408e486763bd
mohanraj0204 <mohan.raj@fourkites.com>
2018-08-26 12:09:09 +0530

    all set uo finished

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,19 +1,14 @@
 require 'spec_helper'
 
 RSpec.describe StaticPagesController do
-
-  describe "GET 'home'" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
+  subject {page}
+  describe "GET home" do
+    before(:each) {visit root_path}
+      it {should have_content('App')}
   end
 
-  describe "GET 'help'" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('mohan')
-    end
+  describe "GET help" do
+    before(:each){visit help_path}      
+      it {should have_content('S')}
   end
-
 end
